3d7a7229033730b6e91716c623d7ad450545a13f
larsonm25 <113474957+larsonm25@users.noreply.github.com>
2023-03-28 15:34:18 -0500

    update to locking code

--- ServoLockingCode/IoTDoggyDoorLock/IoTDoggyDoorLock.ino
+++ ServoLockingCode/IoTDoggyDoorLock/IoTDoggyDoorLock.ino
@@ -4,17 +4,22 @@
 
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
+Servo myservo1;
 
-int pos = 0;    // variable to store the servo position
+int pos = 60;    // variable to store the servo position
+int pos1 = 120;
 
 void setup() {
-  myservo.attach(8);  // attaches the servo on pin 8 to the servo object
-  myservo.write(0);
-  pinMode(7,INPUT);
+  myservo.attach(9);  // attaches the servo on pin 8 to the servo object
+  myservo1.attach(8);
+  myservo1.write(120);
+  myservo.write(60);  
+  pinMode(7,INPUT_PULLUP);
   pinMode(6,INPUT_PULLUP);
   pinMode(4, INPUT_PULLUP);
   pinMode(3,OUTPUT);
   pinMode(11, OUTPUT);
+  digitalWrite(11, HIGH);
 }
 boolean lock = false;
 boolean changed = false;
@@ -47,16 +52,25 @@ void loop() {
 };
 
 void lockDoor(){
-  for (pos = 0; pos <= 60; pos += 1) { // goes from 0 degrees to 180 degrees
+  for (pos = 3; pos <= 60; pos += 1) { // goes from 0 degrees to 180 degrees
       // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(5);   
-  }                    // waits 15ms for the servo to reach the position
+  } 
+  for (pos1 = 25; pos1 <= 120; pos1 += 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+    myservo1.write(pos1);              // tell servo to go to position in variable 'pos'
+    delay(5);   
+  }                   // waits 15ms for the servo to reach the position
 };
 
 void unlockDoor(){
-  for (pos = 60; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+  for (pos = 60; pos >= 3; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(5);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos1 = 120; pos1 >= 25; pos1 -= 1) { // goes from 180 degrees to 0 degrees
+    myservo1.write(pos1);              // tell servo to go to position in variable 'pos'
     delay(5);                       // waits 15ms for the servo to reach the position
   }
 };
