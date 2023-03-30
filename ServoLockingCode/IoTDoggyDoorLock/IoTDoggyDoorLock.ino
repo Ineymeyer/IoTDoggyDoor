794da4b849a4f343acbc77440e4f4aee0a855973
larsonm25 <113474957+larsonm25@users.noreply.github.com>
2023-03-30 14:37:49 -0500

    added 3rd servo

--- ServoLockingCode/IoTDoggyDoorLock/IoTDoggyDoorLock.ino
+++ ServoLockingCode/IoTDoggyDoorLock/IoTDoggyDoorLock.ino
@@ -5,15 +5,19 @@
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 Servo myservo1;
+Servo myservo2;
 
-int pos = 60;    // variable to store the servo position
+int pos = 70;    // variable to store the servo position
 int pos1 = 120;
+int pos2 = 3;
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 8 to the servo object
   myservo1.attach(8);
   myservo1.write(120);
-  myservo.write(60);  
+  myservo.write(70);  
+  myservo2.attach(10);
+  myservo2.write(3);
   pinMode(7,INPUT_PULLUP);
   pinMode(6,INPUT_PULLUP);
   pinMode(4, INPUT_PULLUP);
@@ -52,25 +56,33 @@ void loop() {
 };
 
 void lockDoor(){
-  for (pos = 3; pos <= 60; pos += 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+  for (pos = 3; pos <= 70; pos += 1) {
+    myservo.write(pos);              
     delay(5);   
   } 
-  for (pos1 = 25; pos1 <= 120; pos1 += 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-    myservo1.write(pos1);              // tell servo to go to position in variable 'pos'
+  for (pos1 = 35; pos1 <= 120; pos1 += 1) { 
+      
+    myservo1.write(pos1);              
     delay(5);   
-  }                   // waits 15ms for the servo to reach the position
+  }                   
+ 
+  for (pos2 = 63; pos2 >= 3; pos2 -= 1) { 
+    myservo2.write(pos2);              
+    delay(5);
+  }
 };
 
 void unlockDoor(){
-  for (pos = 60; pos >= 3; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(5);                       // waits 15ms for the servo to reach the position
+  for (pos = 70; pos >= 3; pos -= 1) { 
+    myservo.write(pos);              
+    delay(5);                       
   }
-  for (pos1 = 120; pos1 >= 25; pos1 -= 1) { // goes from 180 degrees to 0 degrees
-    myservo1.write(pos1);              // tell servo to go to position in variable 'pos'
-    delay(5);                       // waits 15ms for the servo to reach the position
+  for (pos1 = 120; pos1 >= 35; pos1 -= 1) { 
+    myservo1.write(pos1);              
+    delay(5);                       
+  }
+   for (pos2 = 3; pos2 <= 63; pos2 += 1) { 
+    myservo2.write(pos2);              
+    delay(5);
   }
 };
